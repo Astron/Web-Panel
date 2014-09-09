748c5b31000b1ad3600952b07ce94e749a341520
bobbybee <j104@rocketmail.com>
2014-09-09 16:57:43 -0400

    Temp fix for lack of Astron websockets

---
+++ ext/bouncer.js
@@ -0,0 +1,30 @@
+// temp WebSocket->Astron bouncer until Astron implements a WebSocket interface
+// deprecate/delete this file ASAP
+// depends on ws module
+// do not bother fixing the long list of bugs in this
+
+var net = require('net');
+var ws = require('ws');
+
+var astron = net.connect({port: 7198},
+	function() {
+		console.log("Connection to Astron established");
+	});
+	
+var wss = new (ws.Server)({
+	port: 8198
+});
+
+var browser;
+
+wss.on('connection', function(ws) {
+	browser = ws;
+	
+	ws.on('message', function(message) {
+		astron.write(message);
+	});
+});	
+	
+astron.on('data', function(d) {
+	ws.send(d, {binary: true, mask: true});
+})
