e53bc9dc13bc9d72e9244b7300f853d11c60f43d
bobbybee <j104@rocketmail.com>
2014-09-09 19:47:10 -0400

    Inital AIR

---
+++ js/air.js
@@ -0,0 +1,32 @@
+var DebugLevel = {
+	SILENCE: 0,
+	FATAL: 1,
+	ERROR: 2,
+	WARN: 3,
+	INFO: 4,
+	DEBUG: 5,
+	TRACE: 6
+}
+
+function AstronInternalRepository(debugLevel) {
+	this.isConnected = false;
+	this.socket = null;
+	this.debugLevel = debugLevel || DebugLevel.WARN;
+}
+
+AstronInternalRepository.prototype.connect = function(host, port) {
+	if(!port) port = 7198;
+	this.socket = new WebSocket("ws://"+host+port);
+	this.socket.onopen = this.connected;
+}
+
+AstronInternalReposiitory.prototype.connected = function(e) {
+	this.isConnected = true;
+	this.log(DebugLevel.INFO, "Connected to Astron");
+}
+
+AstronInternalRepository.prototype.log = function(level, message) {
+	if(this.debugLevel >= level) {
+		console.log(message);
+	}
+}
