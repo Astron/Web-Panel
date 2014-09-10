e044c64241e9ca0f7847fd0a3b7fcffc4678f487
bobbybee <j104@rocketmail.com>
2014-09-10 19:47:51 -0400

    Beginning datagram sending

--- js/Datagram.js
+++ js/Datagram.js
@@ -1,3 +1,41 @@
+// establish a datagram pool for performance reasons
+// (and also to work around deficiencies in JavaScript)
+// note: this means Datagram is not thread-safe, and does not try to be
+// only construct 1 Datagram instance once you have all the needed data
+
+var MAX_DG_SIZE = 0xFFFF;
+
+var dgPoolZero = new Uint8Array(MAX_DG_SIZE+1);
+var dgPool = new Uint8Array(MAX_DG_SIZE+1);
+
+function Datagram() {
+	dgPool.set(dgPoolZero); // clear any old data
+							// TODO: optimize. does this really
+							// need to happen for every datagram?
+							
+	this.buffer = dgPool;
+	this.bufferIndex = 2; // reserve room for initial length field which is added on at the end
+}
+
+Datagram.prototype.writeUInt8 = function(n) {
+	this.buffer[this.bufferIndex++] = n & 0xFF;	
+}
+
+Datagram.prototype.writeUInt16 = function(n) {
+	this.writeUInt8(n & 0x00FF);
+	this.writeUInt16(n & 0xFF00);
+}
+
+Datagram.prototype.writeUInt32 = function(n) {
+	this.writeUInt16(n & 0x0000FFFF);
+	this.writeUInt16(n & 0xFFFF0000);
+}
+
+Datagram.prototype.writeUInt64 = function(n) {
+	this.writeUInt32(n.low);
+	this.writeUInt32(n.high);
+}
+
 // callback is called if the buffer is exhausted but there is more data
 function DatagramIterator(buffer, callback) {
 	this.callback = callback;
@@ -13,13 +51,18 @@ DatagramIterator.prototype.readUInt8 = function() {
 }
 
 DatagramIterator.prototype.readUInt16 = function() {
-	return (this.readUInt8() << 8) | this.readUInt8();
+	return this.readUInt8() | (this.readUInt8() << 8);
 }
 
 DatagramIterator.prototype.readUInt32 = function() {
-	return (this.readUInt16() << 16) | this.readUInt16();
+	return this.readUInt16() | (this.readUInt16() << 16);
 }
 
 DatagramIterator.prototype.readUInt64 = function() {
-	return (this.readUInt32() << 32) | this.readUInt32();
+	return new UInt64(this.readUInt32(), this.readUInt32() << 32);
+}
+
+function UInt64(low, high) {
+	this.low = low;
+	this.high = high;
 }
