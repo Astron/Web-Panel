106012678911899e601d890e10e07d9756677912
bobbybee <j104@rocketmail.com>
2014-09-17 06:54:55 -0400

    General bugfixes in GUIElement, wrapper for creation

--- js/gui.js
+++ js/gui.js
@@ -3,6 +3,11 @@ var GUI = {
 	
 	actionCounter: 0,
 	actions: {},
+	
+	// wrapper function for shorthand
+	create: function(clickable, type, GUIlocation) {
+		return (new GUIElement(clickable, type, GUIlocation));
+	}
 }
 
 function GUIElement(clickable, type, GUIlocation) {
@@ -11,19 +16,19 @@ function GUIElement(clickable, type, GUIlocation) {
 	this.el.style.left = GUIlocation.x;
 	this.el.style.top = GUIlocation.y;
 	
-	GUI.root.appendChild(el);
+	GUI.root.appendChild(this.el);
 }
 
 GUIElement.prototype.label = function(text) {
 	this.el.appendChild(document.createTextNode(text));
-	return this.el;
+	return this;
 }
 
 GUIElement.prototype.action = function(action) {
 	var count = GUI.actionCounter++;
 	GUI.actions[count] = action;
-	element.href = "javascript:GUI.actions["+count+"](e)";
-	return this.el;
+	this.el.href = "javascript:GUI.actions["+count+"]()";
+	return this;
 }
 
 function GUILocation(x, y) {
