49a91dc46ac45282c338f3db8d009e28b37d5da7
bobbybee <j104@rocketmail.com>
2014-09-17 06:48:01 -0400

    Actions

--- js/gui.js
+++ js/gui.js
@@ -1,13 +1,23 @@
 var GUI = {
 	root : null,
-	drawElement : function(type, GUIlocation) {
-		var el = document.createElement("div");
+	drawElement : function(clickable, type, GUIlocation) {
+		var el = document.createElement(clickable ? "a" : "div");
 		el.className = type;
 		el.style.left = GUIlocation.x;
 		el.style.top = GUIlocation.y;
 		GUI.root.appendChild(el);
 		return el;
 	},
+	
+	actionCounter: 0,
+	actions: {},
+	
+	action: function(element, action) {
+		var count = actionCounter++;
+		actions[count] = action;
+		element.href = "javascript:GUI.actions["+count+"](e)";
+	}
+	
 	label: function(element, text) {
 		element.appendChild(document.createTextNode(text));
 		return element;
