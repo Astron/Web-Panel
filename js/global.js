1995ab895d8f5f791caa0a15ef40a209105c6eca
bobbybee <j104@rocketmail.com>
2014-09-20 17:17:29 -0400

    Hierarchy globals

--- js/global.js
+++ js/global.js
@@ -1,5 +1,16 @@
 var air;
 
+// in the future, this should be found by either GUI
+// and/or new Astron discovery mechanisms
+// for now, poke @kestred for feedback
+
+var HierarchyGlobals = {
+	root: 10000,
+	zones: [
+		0
+	]
+}
+
 function startAdmin() {
 	air = new AstronInternalRepository(DebugLevel.TRACE);
 	air.connect("localhost", 8198, "simple_example.dc", function() {
