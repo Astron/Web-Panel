ffbe952e4679ee2741095b9a457e19414ddebcbf
bobbybee <j104@rocketmail.com>
2014-09-20 17:38:48 -0400

    Create hierarchy nodes for discovered objects

--- js/global.js
+++ js/global.js
@@ -28,8 +28,8 @@ function startAdmin() {
 }
 
 function generateHierarchy() {
-	var context = GUI.newRootContext(GUI.location(350, 100));
-	var newHierarchy = new Hierarchy(context);
+	HierarchyGlobals.context = GUI.newRootContext(GUI.location(350, 100));
+	var newHierarchy = new Hierarchy(HierarchyGlobals.context);
 	
 	for(var i = 0; i < HierarchyGlobals.zones.length; ++i) {
 		var zone = HierarchyGlobals.zones[i];
@@ -37,7 +37,7 @@ function generateHierarchy() {
 		HierarchyGlobals.zoneNodes[zone] =
 			 new HierarchyNode(newHierarchy.rootNode, HierarchyGlobals.zones[i], "diamond", function() {
 				 refreshZone(HierarchyGlobals.root, zone);
-			 }, context);
+			 }, HierarchyGlobals.context);
 	}
 	
 	return newHierarchy;
@@ -47,12 +47,16 @@ function refreshZone(parent, zone) {
 	HierarchyGlobals.zoneNodes[zone].removeChildren();
 	
 	air.getZonesObjects(function(numObjects) {
-		alert(numObjects+" object(s) in zone "+zone);
+		//alert(numObjects+" object(s) in zone "+zone);
 	}, parent, [zone]);
 }
 
 function addObjectToHierarchy(obj) {
-	alert(JSON.stringify(obj));
+	var name = obj.dclass[1]+"("+obj.doId+")";
+	new HierarchyNode(HierarchyGlobals.zoneNodes[obj.location.zone], name, "circle", function() {
+		inspect(obj);
+	}, HierarchyGlobals.context);
+	hierarchy.balance();
 }
 
 window.addEventListener("load", startAdmin);
