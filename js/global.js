5dcf2607dce197db0d56ec5d9120b8c362e797a0
bobbybee <j104@rocketmail.com>
2014-09-20 17:16:06 -0400

    Admin panel base

--- js/global.js
+++ js/global.js
@@ -1,8 +1,10 @@
 var air;
 
-window.onload = function() {
+function startAdmin() {
 	air = new AstronInternalRepository(DebugLevel.TRACE);
 	air.connect("localhost", 8198, "simple_example.dc", function() {
 		// connected to Astron
 	});
 }
+
+window.addEventListener("load", startAdmin);
